8f3d18a212b4474c22876c352dd1003ec36b08c8
JuliaGolban <julia.j.shcherban@gmail.com>
2023-11-06 16:16:40 +0200

    first

--- src/js/home.js
+++ src/js/home.js
@@ -2,6 +2,7 @@ import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 gsap.registerPlugin(ScrollTrigger);
+gsap.defaults({ ease: 'none' });
 let speed = 100;
 const widthWindow = window.innerWidth;
 
@@ -12,151 +13,141 @@ ScrollTrigger.create({
   toggleActions: 'restart reverse',
   start: 'top top',
   end: 'bottom 50%+=100px',
-  //   duration: 2,
   scrub: 3,
+});
+
+let screen2 = gsap.timeline();
+ScrollTrigger.create({
+  animation: screen2,
+  trigger: '.screen1',
+  toggleActions: 'restart reverse',
+  start: 'top center',
+  end: '.screen3',
+  scrub: true,
+});
+
+let screen3 = gsap.timeline();
+ScrollTrigger.create({
+  animation: screen3,
+  trigger: '.screen2',
+  toggleActions: 'restart reverse',
+  start: 'top center',
+  end: '.screen3',
+  scrub: true,
+});
+
+let screen4 = gsap.timeline();
+ScrollTrigger.create({
+  animation: screen4,
+  trigger: '.screen3',
+  toggleActions: 'restart reverse',
+  start: 'top center',
+  end: '.screen3',
+  scrub: true,
 });
 
 if (widthWindow < 500) {
   // canvas.to('#car', { x: -1.2 * speed, y: 1 * speed, scale: 2.5 }, 0);
-  canvas.to(
-    '#sun',
-    { x: 9 * speed, y: 1 * speed, delay: 0.03, duration: 2 },
-    0
-  );
-  canvas.to('#cloud-1', { x: -10 * speed, scale: 0.5 }, 0);
-  canvas.to('#cloud-2', { x: 10 * speed, scale: 0.5 }, 0);
-  canvas.to('#builds', { delay: 0.03, scale: 0.9 }, 0);
-  canvas.fromTo('#hills-1', { y: 10 }, { y: -10, scale: 0.8 }, 0);
-  canvas.fromTo('#hills-2', { y: 10 }, { y: -10, scale: 0.8 }, 0);
-  canvas.to(
-    '#tree',
-    { x: -10 * speed, y: -1 * speed, scale: 0.1, delay: 0.03 },
-    0
-  );
-  canvas.to(
-    '#trees',
-    { x: 10 * speed, y: 1 * speed, scale: 0.1, delay: 0.03 },
-    0
-  );
-  canvas.to(
-    '#billboard-1',
-    { x: -10 * speed, y: 1 * speed, scale: 0.1, opacity: 0 },
-    0
-  );
-  first.to(
-    '#billboard-2',
-    { x: 10 * speed, y: 1 * speed, scale: 0.1, opacity: 0 },
-    0
-  );
-  first.to(
-    '#forest-1',
-    { x: -10 * speed, y: -1 * speed, scale: 0.1, delay: 0.03 },
-    0
-  );
-  first.to(
-    '#forest-2',
-    { x: 10 * speed, y: 1 * speed, scale: 0.1, delay: 0.03 },
-    0
-  );
-  canvas.to('#build-1', { x: -10 * speed, scale: 0.1 }, 0);
-  canvas.to(
-    '#build-2',
-    { x: 10 * speed, y: 1 * speed, scale: 0.1, opacity: 0 },
-    0
-  );
-  canvas.to(
-    '#build-3',
-    { x: 10 * speed, y: 1 * speed, scale: 0.1, opacity: 0 },
-    0
-  );
-  canvas.to(
-    '#hospital',
-    { x: 10 * speed, y: 1 * speed, scale: 0.1, opacity: 0 },
-    0
-  );
   // canvas.to('#road', { delay: 0.03, scale: 1.5 }, 0);
 } else {
   // canvas.to('#car', { x: -3.75 * speed, y: 1 * speed, scale: 6.5 }, 0);
-  canvas.to(
-    '#sun',
-    { x: 9 * speed, y: 1 * speed, delay: 0.03, duration: 2 },
-    0
-  );
-  canvas.to('#cloud-1', { x: -10 * speed, scale: 0.5 }, 0);
-  canvas.to('#cloud-2', { x: 10 * speed, scale: 0.5 }, 0);
-  canvas.to('#builds', { delay: 0.03, scale: 0.7 }, 0);
-  canvas.to('#hills-1', { x: 1 * speed, y: -1 * speed }, 0);
-  canvas.to('#hills-2', { x: -1 * speed, y: -1 * speed }, 0);
-  canvas.to(
-    '#tree',
-    { x: -10 * speed, y: -1 * speed, scale: 0.1, delay: 0.03 },
-    0
-  );
-  canvas.to(
-    '#trees',
-    { x: 10 * speed, y: 1 * speed, scale: 0.1, delay: 0.03 },
-    0
-  );
-  canvas.to(
-    '#billboard-1',
-    { x: -10 * speed, y: 1 * speed, scale: 0.1, opacity: 0 },
-    0
-  );
-  canvas.to(
-    '#billboard-2',
-    { x: 10 * speed, y: 1 * speed, scale: 0.1, opacity: 0 },
-    0
-  );
-  canvas.to(
-    '#forest-1',
-    { x: -10 * speed, y: -1 * speed, scale: 0.1, delay: 0.03 },
-    0
-  );
-  canvas.to(
-    '#forest-2',
-    { x: 10 * speed, y: 1 * speed, scale: 0.1, delay: 0.03 },
-    0
-  );
-  canvas.to('#build-1', { x: -10 * speed, scale: 0.1, opacity: 0 }, 0);
-  canvas.to(
-    '#build-2',
-    { x: 10 * speed, y: 1 * speed, scale: 0.1, opacity: 0 },
-    0
-  );
-  canvas.to(
-    '#build-3',
-    { x: 10 * speed, y: 1 * speed, scale: 0.1, opacity: 0 },
-    0
-  );
-  canvas.to(
-    '#hospital',
-    { x: 10 * speed, y: 1 * speed, scale: 0.1, opacity: 0 },
-    0
-  );
   // canvas.to('#road', { y: -1 * speed, delay: 0.03, scale: 1.5 }, 0);
 }
 
-gsap.fromTo(
-  '#car',
-  { x: 1 },
-  {
-    x: 1,
-    transformOrigin: '50% 50%',
-    ease: 'power2.out',
-    scrollTrigger: {
-      toggleActions: 'restart reverse',
-      // toggleClass: 'active',
-      start: 'top top',
-      end: 'bottom 50%+=100px',
-      repeat: -1,
-      yoyo: true,
-      scrub: 4,
-      onEnter: function () {
-        gsap.to('#car', { x: 1 });
-      },
-      onLeave: function () {
-        gsap.to('#car', { x: -1 });
-      },
-    },
-  }
+canvas.to('#sun', { x: 9 * speed, y: 1 * speed, delay: 0.03, duration: 2 }, 0);
+canvas.to('#cloud-1', { x: -10 * speed, scale: 0.5 }, 0);
+canvas.to('#cloud-2', { x: 10 * speed, scale: 0.5 }, 0);
+canvas.to('#builds', { delay: 0.03, scale: 0.7 }, 0);
+canvas.to(
+  '#hills-1',
+  { x: 1 * speed, y: -0.2 * speed, height: '150px', width: '60%' },
+  0
 );
+canvas.to(
+  '#hills-2',
+  { x: -1 * speed, y: -0.2 * speed, height: '150px', width: '60%' },
+  0
+);
+canvas.to(
+  '#tree',
+  {
+    x: 1 * speed,
+    y: -0.1 * speed,
+    scale: 0.1,
+    opacity: 0,
+  },
+  0
+);
+canvas.to(
+  '#trees',
+  {
+    x: -1 * speed,
+    y: -0.2 * speed,
+    scale: 0.1,
+    opacity: 0,
+  },
+  0
+);
+canvas.to(
+  '#billboard-1',
+  { x: 1 * speed, y: -0.2 * speed, scale: 0.1, opacity: 0 },
+  0
+);
+canvas.to(
+  '#billboard-2',
+  { x: -1 * speed, y: -0.2 * speed, scale: 0.1, opacity: 0 },
+  0
+);
+canvas.to(
+  '#forest-1',
+  { x: 1 * speed, y: -0.2 * speed, scale: 0.1, opacity: 0 },
+  0
+);
+canvas.to(
+  '#forest-2',
+  {
+    x: -1 * speed,
+    y: -0.2 * speed,
+    scale: 0.1,
+    delay: 0.02,
+    opacity: 0,
+  },
+  0
+);
+canvas.to(
+  '#build-1',
+  { x: 1 * speed, y: -0.2 * speed, scale: 0.1, opacity: 0 },
+  0
+);
+canvas.to(
+  '#build-2',
+  { x: -1 * speed, y: -0.2 * speed, scale: 0.1, opacity: 0 },
+  0
+);
+canvas.to(
+  '#build-3',
+  { x: -1 * speed, y: -0.2 * speed, scale: 0.1, opacity: 0 },
+  0
+);
+canvas.to(
+  '#hospital',
+  { x: 1 * speed, y: -0.2 * speed, scale: 0.1, opacity: 0 },
+  0
+);
+
+gsap.to('#car', {
+  keyframes: {
+    '0%': { x: 0, y: 0 },
+    '20%': { x: '-2px', y: '2px' },
+    '40%': { x: '-2px', y: '-2px' },
+    '60%': { x: '2px', y: '2px' },
+    '80%': { x: '2px', y: '-2px' },
+    '100%': { x: 0, y: 0 },
+  },
+  duration: 1,
+  repeat: -1,
+  scrollTrigger: {
+    start: 'top top',
+    end: 'bottom 50%+=100px',
+  },
+});
